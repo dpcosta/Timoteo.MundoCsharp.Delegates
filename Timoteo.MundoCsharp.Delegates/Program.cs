d2ecc7d08a389aad171fc5837122b2632c9ddb04
Daniel Portugal <dpcosta@gmail.com>
2019-02-21 16:00:12 -0300

    Adicionar arquivos de projeto.

---
+++ Timoteo.MundoCsharp.Delegates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Timoteo.MundoCsharp.Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
